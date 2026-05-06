e151422d35b6f27faad1f0ad2c136978d7eb314e
dn <dknomura@gmail.com>
2026-05-06 02:31:16 -0700

    clean up map

--- notebooks/sb79_map.py
+++ notebooks/sb79_map.py
@@ -10,14 +10,13 @@
 
 import marimo
 
-__generated_with = "0.23.4"
+__generated_with = "0.23.5"
 app = marimo.App()
 
 
 @app.cell
 def _():
     import folium
-    import marimo as mo
     from folium.plugins import MarkerCluster, VectorGridProtobuf
 
     from arcgis_paginate import (
@@ -28,10 +27,6 @@ def _():
     STOPS_URL = (
         "https://services3.arcgis.com/NaFf4UaPo3IgQXqn/ArcGIS/rest/services/sb79_transit_stations/FeatureServer/0/query"
     )
-
-    # https://vectortileservices3.arcgis.com/NaFf4UaPo3IgQXqn/arcgis/rest/services/sb79_transit_parcels/VectorTileServer/tile/12/704/1634.pbf
-
-    # https://vectortileservices3.arcgis.com/NaFf4UaPo3IgQXqn/arcgis/rest/services/sb79_transit_parcels/VectorTileServer/tile/7/49/21.pbf
     return (
         MarkerCluster,
         PARCELS_URL,
@@ -39,7 +34,6 @@ def _():
         VectorGridProtobuf,
         fetch_from_arcgis,
         folium,
-        mo,
     )
 
 
@@ -51,9 +45,10 @@ def _(STOPS_URL, fetch_from_arcgis):
 
 
 @app.cell
-def _(MarkerCluster, folium, stops):
+def _(MarkerCluster, PARCELS_URL, VectorGridProtobuf, folium, stops):
     m = folium.Map(location=[34.0617140033952, -118.314146442073], tiles="CartoDB Positron", zoom_start=10)
 
+    VectorGridProtobuf(PARCELS_URL, "folium_layer_name").add_to(m)
     cluster = MarkerCluster(disable_clustering_at_zoom=10).add_to(m)
     for _, _row in stops.iterrows():
         color = "blue" if _row["Tier"] == 2 else "red"
@@ -75,51 +70,7 @@ def _(MarkerCluster, folium, stops):
             ).add_to(m)
         )
     m
-    return (m,)
-
-
-@app.cell
-def _(PARCELS_URL, VectorGridProtobuf, m):
-    VectorGridProtobuf(PARCELS_URL, "folium_layer_name").add_to(m)
-    m
     return
-
-
-@app.cell
-def _(m, mo):
-    map_widget = mo.ui.map(m)
-    map_widget
-    return (map_widget,)
-
-
-@app.cell
-def _(map_widget):
-    import requests
-    from marimo import mo
-
-    # This will run every time the map is clicked
-    if map_widget.value:
-        lat, lon = map_widget.value["lat"], map_widget.value["lon"]
-
-        # Use the FeatureServer's /query endpoint (assuming geometryType=esriGeometryPoint)
-        query_url = (
-            "https://services3.arcgis.com/NaFf4UaPo3IgQXqn/arcgis/rest/services/sb79_transit_parcels/FeatureServer/0/query"
-        )
-        params = {
-            "geometry": f"{lon},{lat}",
-            "geometryType": "esriGeometryPoint",
-            "spatialRel": "esriSpatialRelIntersects",
-            "outFields": "*",  # get all attributes
-            "returnGeometry": "false",  # we only need attributes
-            "f": "geojson",
-        }
-        resp = requests.get(query_url, params=params)
-        if resp.ok and resp.json()["features"]:
-            parcel = resp.json()["features"][0]["properties"]
-            mo.md(f"**Parcel info:**\n```json\n{parcel}\n```")
-        else:
-            mo.md("No parcel found at that point.")
-    return (mo,)
 
 
 if __name__ == "__main__":
